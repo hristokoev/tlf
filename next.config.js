b46baa207bc2dcf1cb50f5032e4839aec139f594
Hristo Koev <iamhristokoev@gmail.com>
2025-08-19 15:03:06 +0300

    fix: nextjs images

--- next.config.js
+++ next.config.js
@@ -2,18 +2,19 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 import redirects from './redirects.js'
 
-const NEXT_PUBLIC_SERVER_URL =
-  process.env.NEXT_PUBLIC_SERVER_URL ||
-  (process.env.VERCEL_PROJECT_PRODUCTION_URL
-    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-    : process.env.__NEXT_PRIVATE_ORIGIN || 'http://localhost:3000')
+const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+  : undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+
+const WWW_SERVER_URL = NEXT_PUBLIC_SERVER_URL.replace('https://', 'https://www.')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL].map((item) => {
+      ...[NEXT_PUBLIC_SERVER_URL, WWW_SERVER_URL].map((item) => {
         const url = new URL(item)
+
         return {
           hostname: url.hostname,
           protocol: url.protocol.replace(':', ''),
